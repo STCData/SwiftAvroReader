6e05c03703dd0161c3765453f7cb8ad3797574bd
stc <standardtemplateconstruct2022@gmail.com>
2023-03-01 12:52:16 +0200

    initial

---
+++ Sources/SwiftAvroReader/SwiftAvroReader.swift
@@ -0,0 +1,6 @@
+public struct SwiftAvroReader {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ avroread/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  avroread
+//
+//  Created by standard on 3/1/23.
+//
+
+import Foundation
+
+print("Hello, World!")
+
